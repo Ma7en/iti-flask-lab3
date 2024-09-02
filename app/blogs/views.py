e4aa5e2e504a61c4f84e4de2a21d2d1af3a735a1
Ma7en <m9ee9m@gmail.com>
2024-09-02 19:51:34 +0300

    edit category

--- app/blogs/views.py
+++ app/blogs/views.py
@@ -1,4 +1,4 @@
-from app.models import Blogs, db
+from app.models import Blogs, db, Categories
 from flask import render_template, request, redirect, url_for, Blueprint
 from app.blogs import blogs_blueprint
 from app.blogs.forms import BlogsForm
@@ -11,26 +11,27 @@ def blogs_list():
     return render_template("blogs/list.html", blogs=blogs)
 
 
-# @blogs_blueprint.route("create", endpoint="create", methods=["GET", "POST"])
-# def blogs_create():
-#     # print(request.method, request.form)
-#     if request.method == "POST":
-#         blog = Blogs(
-#             name=request.form["name"],
-#             description=request.form["description"],
-#             image=request.form["image"],
-#         )
-#         db.session.add(blog)
-#         db.session.commit()
-#         return redirect(blog.show_url)
-#     return render_template("blogs/create.html")
-
-
 @blogs_blueprint.route("create", endpoint="create", methods=["GET", "POST"])
 def blogs_create():
-    form = BlogsForm()
+    categories = Categories.query.all()
+    if request.method == "POST":
 
-    return render_template("blogs/forms/create.html", form=form)
+        blog = Blogs(
+            name=request.form["name"],
+            description=request.form["description"],
+            image=request.form["image"],
+            category_id=request.form["category_id"],
+        )
+        db.session.add(blog)
+        db.session.commit()
+        return redirect(blog.show_url)
+    return render_template("blogs/create.html", categories=categories)
+
+
+# @blogs_blueprint.route("create", endpoint="create", methods=["GET", "POST"])
+# def blogs_create():
+#     form = BlogsForm()
+#     return render_template("blogs/forms/create.html", form=form)
 
 
 @blogs_blueprint.route("<int:id>/update", endpoint="update", methods=["GET", "POST"])
@@ -54,6 +55,7 @@ def blogs_update(id):
 @blogs_blueprint.route("<int:id>", endpoint="show")
 def blog_show(id):
     blog = db.get_or_404(Blogs, id)
+    # category = db.get_or_404(Categories, blog.category_id)
     return render_template("blogs/show.html", blog=blog)
 
 
--- app/models.py
+++ app/models.py
@@ -4,38 +4,12 @@ from flask import url_for
 db = SQLAlchemy()
 
 
-class Blogs(db.Model):
-    __tablename__ = "blogs"
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=True)
-    description = db.Column(db.String(5000), nullable=True)
-    image = db.Column(db.String(250), nullable=True)
-
-    def __str__(self):
-        return f"{self.name}"
-
-    @property
-    def image_url(self):
-        return url_for("static", filename=f"assets/images/blogs/{self.image}")
-
-    @property
-    def show_url(self):
-        return url_for("blogs.show", id=self.id)
-
-    @property
-    def update_url(self):
-        return url_for("blogs.update", id=self.id)
-
-    @property
-    def delete_url(self):
-        return url_for("blogs.delete", id=self.id)
-
-
 class Categories(db.Model):
     __tablename__ = "categories"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=True)
     image = db.Column(db.String(250), nullable=True)
+    blogs = db.relationship("Blogs", backref="category", lazy=True)
 
     def __str__(self):
         return f"{self.name}"
@@ -55,3 +29,31 @@ class Categories(db.Model):
     @property
     def delete_url(self):
         return url_for("categories.delete", id=self.id)
+
+
+class Blogs(db.Model):
+    __tablename__ = "blogs"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=True)
+    description = db.Column(db.String(5000), nullable=True)
+    image = db.Column(db.String(250), nullable=True)
+    category_id = db.Column(db.Integer, db.ForeignKey("categories.id"), nullable=True)
+
+    def __str__(self):
+        return f"{self.name}"
+
+    @property
+    def image_url(self):
+        return url_for("static", filename=f"assets/images/blogs/{self.image}")
+
+    @property
+    def show_url(self):
+        return url_for("blogs.show", id=self.id)
+
+    @property
+    def update_url(self):
+        return url_for("blogs.update", id=self.id)
+
+    @property
+    def delete_url(self):
+        return url_for("blogs.delete", id=self.id)
