880799ce52e8c08352e2b9093ae8f9d76d33c509
Ma7en <m9ee9m@gmail.com>
2024-09-06 09:04:36 +0300

    edit updata and create categories

--- app/categories/views.py
+++ app/categories/views.py
@@ -1,12 +1,16 @@
 from flask import render_template, request, redirect, url_for, Blueprint
 from app.models import Categories, db
 from app.categories import categories_blueprint
+from app.categories.forms import CategoriesForm
+from werkzeug.utils import secure_filename
+import os, datetime
+
 
 # from app.blogs.forms import BlogsForm
 
 
 # =================================================================================================
-# *** list blogs ***
+# *** list categories ***
 @categories_blueprint.route("/", endpoint="list")
 def categories_list():
     categories = Categories.query.all()
@@ -14,22 +18,51 @@ def categories_list():
 
 
 # =================================================================================================
-# *** create blog ***
+# *** create category ***
+# @categories_blueprint.route("create", endpoint="create", methods=["GET", "POST"])
+# def categories_create():
+#     if request.method == "POST":
+#         category = Categories(
+#             name=request.form["name"],
+#             image=request.form["image"],
+#         )
+#         db.session.add(category)
+#         db.session.commit()
+#         return redirect(category.show_url)
+#     return render_template("categories/forms/create.html")
+
+
 @categories_blueprint.route("create", endpoint="create", methods=["GET", "POST"])
 def categories_create():
+    form = CategoriesForm()
+    date = datetime.datetime.now()
+
     if request.method == "POST":
-        category = Categories(
-            name=request.form["name"],
-            image=request.form["image"],
-        )
-        db.session.add(category)
-        db.session.commit()
-        return redirect(category.show_url)
-    return render_template("categories/forms/create.html")
+        if form.validate_on_submit():
+            image_name = None
+            if request.files.get("image"):
+                image = form.image.data
+                image_name = secure_filename(image.filename)
+                con_name = f"{date.day}-{date.hour}-{date.minute}-{image_name}"
+                image.save(
+                    os.path.join("static/assets/images/blogs/", con_name)
+                )  # image_name
+
+            data = dict(request.form)
+            del data["csrf_token"]
+            del data["submit"]
+            # save only image name
+            data["image"] = con_name
+            blog = Categories(**data)
+            db.session.add(blog)
+            db.session.commit()
+
+            return redirect(blog.show_url)
+    return render_template("blogs/forms/create.html", form=form)
 
 
 # =================================================================================================
-# *** update blog ***
+# *** update category ***
 @categories_blueprint.route(
     "<int:id>/update", endpoint="update", methods=["GET", "POST"]
 )
